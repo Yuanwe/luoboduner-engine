eb77ed67b5b23888ccda11165817c323e6077c1b
zhouy <rememberber@163.com>
2017-11-10 18:49:24 +0800

    init web-admin config

--- luoboduner-web-admin/src/main/java/com/luoboduner/web/admin/LuobodunerWebAdminApplication.java
+++ luoboduner-web-admin/src/main/java/com/luoboduner/web/admin/LuobodunerWebAdminApplication.java
@@ -1,9 +1,11 @@
 package com.luoboduner.web.admin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.luoboduner.dao.mapper")
 public class LuobodunerWebAdminApplication {
 
 	public static void main(String[] args) {
